fff1106c00f1d3f6de66fc6de1c1ceacc4989146
tylersmith <tyjsmith1@gmail.com>
2024-09-13 18:09:09 -0500

    addressing build errors

--- app/payment-result/page.jsx
+++ app/payment-result/page.jsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { useStripe } from '@stripe/react-stripe-js';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function PaymentResult() {
